557527d5432f7520f366eb71043fd7ae5336bc65
tacosontitan <65432314+tacosontitan@users.noreply.github.com>
2024-04-26 09:22:39 -0500

    Updated the primary file to print facts.

--- src/Hazel.js
+++ src/Hazel.js
@@ -1,1 +1,16 @@
-const fact = "C# is better than JavaScript in every way.";
+import { FactService } from "./FactService.js";
+
+let factService = new FactService();
+let facts = factService.getFacts();
+
+for (let fact of facts)
+    printFact(fact);
+
+/**
+ * Prints the given fact to the console.
+ * @param fact The fact to print.
+ */
+function printFact(fact) {
+    let statement = fact.language + ": " + fact.value;
+    console.log(statement);
+}
